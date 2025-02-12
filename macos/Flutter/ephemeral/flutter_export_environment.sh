65bd34f3555f750d44560293a4f7a8363cc14c0e
Hasali Sooriyaarachchi <155374073+HasaliM23@users.noreply.github.com>
2025-02-12 07:37:46 -0800

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aylana_weddings/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aylana_weddings/Documents/Flutter_Projects/EDUCONNECT 5/educonnectfinal"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
